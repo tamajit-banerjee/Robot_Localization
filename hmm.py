e1b04bd4517bb086e3651c20a408682638b3a3f5
tamajit-banerjee <tamajitbuba3@gmail.com>
2022-02-27 20:19:25 +0530

    Debugging done

--- hmm.py
+++ hmm.py
@@ -25,6 +25,7 @@ class RobotModel():
         init_pos = np.random.randint(1, X-1), np.random.randint(1, Y-1)
         while self.grid[init_pos[1]][init_pos[0]] == 1:
             init_pos = np.random.randint(1, X-1), np.random.randint(1, Y-1)
+        # init_pos = 2,2
         self.pos = init_pos
         self.R_max = R_max
         self.T = 0
@@ -45,7 +46,7 @@ class RobotModel():
         grid = 255*np.ones((grid_len*self.rows, grid_len*self.cols, 3))
         for y in range(self.rows):
             for x in range(self.cols):
-                if self.pos[0] == x and self.pos[1] == y:
+                if self.pos[0] == y and self.pos[1] == x:
                     center = (x*grid_len + grid_len//2, y * grid_len+ grid_len//2)
                     cv2.circle(grid, center, grid_len//3, color=(0,255,0), thickness=10)
                     continue
@@ -100,7 +101,7 @@ class RobotModel():
 
 class estimator():
     def __init__(self, X, Y, R_max, grid, initial_observation):
-        self.grid = grid
+        self.grid = copy.deepcopy(grid)
         self.cols = X
         self.rows = Y
         self.dim =  X * Y
@@ -113,10 +114,10 @@ class estimator():
             "W": (-1, 0),
         }
         self.order = {
-            "N": 3,
-            "S": 2,
-            "E": 1,
-            "W": 0,
+            "N": 0,
+            "S": 1,
+            "E": 2,
+            "W": 3,
         }
 
         ## using initial distribution as uniform distribution
@@ -125,12 +126,14 @@ class estimator():
         count_valid_cells = 0
         for i in range(self.rows):
             for j in range(self.cols):
-                if ( grid[i][j] == 1 ) :
+                if ( grid[i][j] == 0 ) :
                     count_valid_cells += 1
         for i in range(self.rows):
             for j in range(self.cols):
-                if ( grid[i][j] == 1 ):
+                if ( grid[i][j] == 0 ):
                     self.initial_distribution[i*self.rows+j] = 1/count_valid_cells
+
+        print("Initial distribution :: ",self.initial_distribution)
 
         ## calculating probability P(Xt+1|Xt) = Sigma P(Xt+1|Ut,Xt) P(Ut|Xt)
         # self.transition_probability = np.zeros((self.dim,self.dim))
@@ -141,48 +144,64 @@ class estimator():
             if grid[y][x] == 0:
                 moves = [(1, 0), (0, 1), (-1, 0), (0, -1)]
                 allowed_moves = []
-                for i in range(len(moves)):
-                    m = moves[i]
+                for j in range(len(moves)):
+                    m = moves[j]
                     if self.grid[y+m[1]][x+m[0]] == 0:
-                        allowed_moves.append(i)
+                        allowed_moves.append(m)
                 moves_possible = len(allowed_moves)
-                for move_id in allowed_moves:
-                    y_new = y + moves[move_id][1] 
-                    x_new = x + moves[move_id][0]
+                for m in allowed_moves:
+                    y_new = y + m[1] 
+                    x_new = x + m[0]
                     self.transition_probability[y_new * self.rows + x_new][i] = 1/moves_possible 
         
+        print("Transition_probability :: ",self.transition_probability) 
+
         ## calculating probability P(Ot|Xt)  1 == Close , 0 == Far encoded as 1001 
         # self.observation_probablity = np.zeros((16,self.dim))
-        self.observation_probablity = [[0.0 for i in range(self.dim)] for j in range(self.dim)]
+        self.observation_probablity = [[0.0 for i in range(self.dim)] for j in range(16)]
 
         for i in range (self.dim):
             x = i % self.rows
             y = i // self.rows
-            if grid[y][x] == 0:
+            if self.grid[y][x] == 0:
                 for j in range(16):
                     self.observation_probablity[j][i] = 1.0
                 for sensor in ["N", "S", "E", "W"]:
-                    i = 0
+                    ij = 1
                     dir = self.directions[sensor]
-                    while self.grid[y+i*dir[1]][x + i*dir[0]] == 0:
-                        i += 1
-                    prob = 1
-                    if i < self.R_max:
-                        prob = (i-1)/(self.R_max - 1)
+                    while self.grid[y+ij*dir[1]][x + ij*dir[0]] == 0:
+                        ij += 1
+                    prob = 1.0
+                    if ij < self.R_max:
+                        prob = (ij-1)/(self.R_max - 1)
                     for j in range(16):
-                        if  ( ( (j>>self.order[sensor]) & 1 ) == 1 ) :
+                        if( ( j & (1 << self.order[sensor] ) ) > 0 ) :
+                            if i == 6:
+                                print( j , sensor)
                             self.observation_probablity[j][i] *= (1.0 - prob)
                         else:
+                            if ( j == 0 ):
+                                print(i , prob, ij)
                             self.observation_probablity[j][i] *= prob
         
+        print("Observation_probability :: ",self.observation_probablity) 
+        
         self.init_observation = self.encode_observation(initial_observation)
+        print(self.init_observation)
         self.observations = [self.init_observation]
         current_estimate = self.initial_distribution
-
+        sum = 0.0
         for i in range(self.dim):
             x = i % self.rows
             y = i // self.rows
             current_estimate[i] *= self.observation_probablity[self.init_observation][i]
+            sum += current_estimate[i]
+        for i in range(self.dim):
+            x = i % self.rows
+            y = i // self.rows
+            current_estimate[i] /= sum
+
+        print("Current_Estimate :: ",current_estimate)
         
         self.estimates = [current_estimate]
         self.viterbi_values = [current_estimate]
@@ -207,9 +226,9 @@ class estimator():
     def update_most_likely_estimate(self):
         new_viterbi_values = [0.0 for i in range(self.dim)]
         new_viterbi_best_prev_state = [None for i in range(self.dim)]
-        for cur_state in self.dim:
+        for cur_state in range(self.dim):
             max_tr_prob , prev_st_selected = None , None
-            for prev_state in self.dim:
+            for prev_state in range(self.dim):
                 tr_prob = self.viterbi_values[-1][prev_state] * self.transition_probability[cur_state][prev_state]
                 if max_tr_prob == None or tr_prob > max_tr_prob:
                     max_tr_prob = tr_prob
@@ -222,7 +241,7 @@ class estimator():
     
     def update_viterbi_sequence(self):
         self.viterbi_sequence = []
-        last_ver = [argmax(self.viterbi_values[-1])]
+        last_ver = argmax(self.viterbi_values[-1])
         cur_time = self.T
         while( cur_time != -1):
             self.viterbi_sequence.append(last_ver)
@@ -239,19 +258,31 @@ class estimator():
             current_values[cur_state] = tr_prob * self.observation_probablity[self.observations[-1]][cur_state]
             current_value_sum += current_values[cur_state]
 
+        # print(current_value_sum)
+
         assert(current_value_sum != 0.0)
 
-        for cur_state in self.dim:
+        for cur_state in range(self.dim):
             current_values[cur_state] /= current_value_sum
             
         self.estimates.append(current_values)
+
+    def get_current_estimate(self):
+        return (argmax(self.estimates[-1])//self.rows , argmax(self.estimates[-1]) % self.rows )
         
 
 os.makedirs("grids_img", exist_ok=True)
 
+X , Y = 7 , 7
+R_max = 2
+n_walls = 0
+model = RobotModel(X, Y, R_max,n_walls)
 
-model = RobotModel(7, 7, n_walls=4, R_max=2)
-estimate = estimator(7,7,2,copy.deepcopy(model.grid),model.make_observation())
+observation = model.make_observation()
+# observation = {'N': 'Far', 'S': 'Far', 'E': 'Close', 'W': 'Far'}
+print( observation )
+estimate = estimator(X, Y, R_max,copy.deepcopy(model.grid),observation)
+model.visualise_grid("grids_img/{}.png".format(-1))
 for i in range(100):
     model.visualise_grid("grids_img/{}.png".format(i))
     model.make_random_move()
@@ -259,3 +290,5 @@ for i in range(100):
     observation = model.make_observation()
     print( observation )
     estimate.update(observation)
+    print(estimate.estimates[-1])
+    # print(estimate.get_current_estimate())
